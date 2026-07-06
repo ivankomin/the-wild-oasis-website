4e4a5392342b9f4404d15c7c6661c124afbe01a0
Ivan Komin <ivankom0531@gmail.com>
2026-07-06 18:59:27 +0300

    added some styling

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,14 @@
-export default function Page() {
+export const metadata = {
+  title: "Cabins",
+}
+
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
   return (
     <div>
-      <h1>cabins</h1>
+      <h1>Cabins Page</h1>
     </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,27 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
+
+import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
-  description: "Welcome to paradise",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="text-primary-100 bg-primary-950 min-h-screen">
         <header>
           <Logo />
         </header>
         <Navigation />
         <main>{children}</main>
+        <footer>Copyright &copy; The Wild Oasis</footer>
       </body>
     </html>
   );
