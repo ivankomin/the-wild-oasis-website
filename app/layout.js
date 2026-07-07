067ce4c9ce4f66a6622e407441ecd862fc88a19f
Ivan Komin <ivankom0531@gmail.com>
2026-07-07 17:38:50 +0300

    added more styling + nested layouts

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
+import { Josefin_Sans } from "next/font/google";
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 
 export const metadata = {
   title: {
@@ -15,13 +17,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="text-primary-100 bg-primary-950 min-h-screen">
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright &copy; The Wild Oasis</footer>
+      <body
+        className={`${josefin.className}  text-primary-100 bg-primary-950 min-h-screen flex flex-col antialiased relative`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
