e423ca8f45b2e11482c69a44eb6d63e769e1acee
Ivan Komin <ivankom0531@gmail.com>
2026-09-23 20:22:11 +0300

    more server actions

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,37 @@
 "use server";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
 
-import { signIn, signOut } from "./auth";
+export async function updateProfile(formData) {
+  const session = await auth();
+  if (!session) {
+    throw new Error("User is not authenticated");
+  }
+
+  const [nationality, countryFlag] = formData.get("nationality")?.split("%"); // Split the value to get the country name
+  const nationalID = formData.get("nationalID");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) {
+    throw new Error("Invalid national ID format");
+  }
+
+  const updateData = { nationality, countryFlag, nationalID };
+
+  const {data, error} = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId)
+    .select()
+    .single();
+    
+  if (error) {
+    console.error(error);
+    throw new Error("Profile update failed");
+  }
+
+  revalidatePath("/account/profile");
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
